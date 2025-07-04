d98cd4329ce5161ddd7f37eb4fd6bf727cc48e94
AlexThukynov1 <alexthukynov@gmail.com>
2025-07-04 19:13:53 +0300

    add tech section

--- src/componetns/Tech.jsx
+++ src/componetns/Tech.jsx
@@ -4,8 +4,19 @@ import {technologies} from "../constants/index.js";
 
 const Tech = () => {
     return (
-        <></>
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+            {technologies.map((technology, index) => (
+                <div
+                    className="w-28 h-28"
+                    key={index}
+                >
+                    <BallCanvas
+                        icon={technology.icon}
+                    />
+                </div>
+            ))}
+        </div>
     )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "techologies")
