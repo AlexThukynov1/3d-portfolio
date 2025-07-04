82d5b8cf58bb7b942e3465ac374744f86dfddc0c
AlexThukynov1 <alexthukynov@gmail.com>
2025-07-04 19:00:39 +0300

    minor corrections

--- src/componetns/Tech.jsx
+++ src/componetns/Tech.jsx
@@ -1,3 +1,7 @@
+import {BallCanvas} from '../canvas/index.js'
+import {SectionWrapper} from '../hoc'
+import {technologies} from "../constants/index.js";
+
 const Tech = () => {
     return (
         <></>
